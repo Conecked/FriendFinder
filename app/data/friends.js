f1522c3b9dd18f4df9570507f961d9d82ad3ce62
Armando <xxnowonxx@gmail.com>
2018-08-10 19:59:31 -0700

    updated survey files

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,16 +10,16 @@ var friendArray = [
     name: "Bob",
     photo: "https://vignette.wikia.nocookie.net/spongebob/images/a/ac/Spongebobwithglasses.jpeg/revision/latest?cb=20121014113150",
     scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
     ]
 }
 ];
